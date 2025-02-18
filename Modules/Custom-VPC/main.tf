5abd905b8bc2c6b089dff1f8ad1b7aa8aa506a74
Akshay Kumar <77635736+akshay9811@users.noreply.github.com>
2025-02-18 22:57:45 +0530

    Update main.tf

--- Modules/Custom-VPC/main.tf
+++ Modules/Custom-VPC/main.tf
@@ -1,3 +1,4 @@
+}
 resource "aws_vpc" "myVPC" {
   cidr_block = "10.0.0.0/16"
 }
@@ -17,7 +18,7 @@ resource "aws_internet_gateway" "myig" {
   vpc_id = aws_vpc.myVPC.id
 }
 resource "aws_route_table" "RT" {
-  vpc_id = aws_vpc.myVPC
+  vpc_id = aws_vpc.myVPC.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.myig.id
